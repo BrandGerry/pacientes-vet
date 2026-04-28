08b75a375061dbf6b4774add71050b090d729c6b
BrandGerry <91706030+BrandGerry@users.noreply.github.com>
2026-04-28 15:25:06 -0600

    ADD:FISRT COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { PatiensList } from "./components/PatiensList";
+import PatientForm from "./components/PatientForm";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de pacientes
+          <span className="text-indigo-700"> Veterinaria</span>
+        </h1>
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatiensList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
